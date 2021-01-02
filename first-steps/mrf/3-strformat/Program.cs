74eb5af2131362b3e4184415c8d735daa06b50c3
Riza <ed.fairuzz@gmail.com>
2021-01-02 14:22:30 +0700

    3

---
+++ first-steps/mrf/3-strformat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _3_strformat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello\nWorld!");
+            Console.WriteLine("Hello\tWorld!");
+            Console.WriteLine("Hello \"World\"!"); //Escape sequence
+            Console.WriteLine("c:\\source\\repos");
+            Console.WriteLine("Generating invoices for customer \"ABC Corp\" ...\n");
+            Console.WriteLine("Invoice: 1021\t\tComplete!");
+            Console.WriteLine("Invoice: 1022\t\tComplete!");
+            Console.WriteLine("\nOutput Directory:\t");
+            Console.Write(@"c:\invoices");
+
+            // To generate Japanese invoices:
+            // Nihon no seikyū-sho o seisei suru ni wa:
+            Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+            Console.WriteLine(@"c:\invoices\app.exe -j");
+
+            Console.WriteLine(@"   c:\source\repos   
+                (this is where your code goes)"); //verbatim string literal
+            Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+        }
+    }
+}
---
+++ first-steps/mrf/31-strconcatenation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _31_strconcatenation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Humeyra";
+            string message = "Hello " + firstName;
+            Console.WriteLine(message);
+
+            firstName = "Riza";
+            string greeting = "Hello";
+            Console.WriteLine(greeting + " " + firstName + "!");
+
+            message = greeting + " " + firstName + "!";
+            Console.WriteLine(message);
+            Console.WriteLine($"{greeting} {firstName}!");
+            Console.WriteLine($"Hello {firstName}!");
+
+            string projectName = "First-Project";
+            Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+            projectName = "ACME";
+            string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+            Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+                        
+            string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+            string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+            Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+        }
+    }
+}
