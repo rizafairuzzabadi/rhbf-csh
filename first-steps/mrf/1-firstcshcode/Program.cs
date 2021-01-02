e5067dd1d866f9865b295478d01299a279b7b545
Riza <ed.fairuzz@gmail.com>
2021-01-02 13:12:32 +0700

    1

---
+++ first-steps/mrf/1-firstcshcode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1_firstcshcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //console.WriteLine("Hello World!"); - Error
+            // Console.WriteLine("Hello World!");
+            Console.Write("Congratulations!");
+            Console.Write(" ");
+            Console.Write("You wrote your first lines of code!");
+        }
+    }
+}
