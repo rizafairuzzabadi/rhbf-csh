491d84d27b7c543f5b0c9daeeed5625423ed327f
Riza <ed.fairuzz@gmail.com>
2021-01-02 15:32:43 +0700

    6

---
+++ first-steps/mrf/6-ifElse/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace _6_ifElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int roll1 = dice.Next(1, 7);
+            int roll2 = dice.Next(1, 7);
+            int roll3 = dice.Next(1, 7);
+            int total = roll1 + roll2 + roll3;
+            Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+            if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))   //OR
+            {
+                Console.WriteLine("You rolled doubles! +2 bonus to total!");
+                total += 2;
+            }
+
+            if ((roll1 == roll2) && (roll2 == roll3)) //AND
+            {
+                Console.WriteLine("You rolled triples! +6 bonus to total!");
+                total += 6;
+            }
+
+            if (total >= 16)
+            {
+                Console.WriteLine("You win a new car!");
+            }
+            else if (total >= 10)
+            {
+                Console.WriteLine("You win a new laptop!");
+            }
+            else if (total == 7)
+            {
+                Console.WriteLine("You win a trip for two!");
+            }
+            else
+            {
+                Console.WriteLine("You win a kitten!");
+            }
+
+            //quick brown fox
+            string message = "The quick brown fox jumps over the lazy dog.";
+            bool result = message.Contains("dog");
+            Console.WriteLine(result);
+
+            if (message.Contains("fox"))
+            {
+                Console.WriteLine("What does the fox say?");
+            }
+
+            //challenge
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12);
+            int discountPercentage = 0;
+            if (daysUntilExpiration >= 10)
+            {
+                Console.WriteLine("Your subscription will expire soon. Renew Now!");
+            }
+            else if (daysUntilExpiration <= 5)
+            {
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+                discountPercentage = 10;
+            }
+            else if (daysUntilExpiration == 1)
+            {
+                Console.WriteLine("Your subscription expires within a day!");
+                discountPercentage = 20;
+            }
+            if (daysUntilExpiration == 0)
+            {
+                Console.WriteLine("Your subscription has expired.");
+            }
+            if (discountPercentage > 0)
+            {
+                Console.WriteLine($"Renew now and save {discountPercentage}%.");
+            }
+
+        }
+    }
+}
