cbbc0ebd259305c1c83fef651bb713cb98e8e729
ahumeyraboz <76833914+ahumeyraboz@users.noreply.github.com>
2021-01-03 15:10:34 +0300

    iki

---
+++ first-steps/ahb/iki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace iki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
