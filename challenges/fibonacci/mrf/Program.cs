5821b213a9a241aa18b2e17e7d0480c9a2c91f29
Muhammad Riza Fairuzzabadi <riza@fairuzzabadi.com>
2021-01-14 01:06:56 +0700

    fix

--- challenges/fibonacci/mrf/Program.cs
+++ challenges/fibonacci/mrf/Program.cs
@@ -25,12 +25,5 @@ namespace mrf
             }
             return result[n];
         }
-        /*         static int fibonacci(int n)
-                {
-                    if (n <= 1)
-                        return n;
-                    return fibonacci(n - 1) + fibonacci(n - 2);
-                } */
-
     }
 }
