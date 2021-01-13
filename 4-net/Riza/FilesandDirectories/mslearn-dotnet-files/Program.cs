6838e038f8bdf17ec3bee41db2e0d2c5ca3fc11d
Muhammad Riza Fairuzzabadi <riza@fairuzzabadi.com>
2021-01-14 01:06:01 +0700

    h

---
+++ 4-net/Riza/FilesandDirectories/mslearn-dotnet-files/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace files_module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+            var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+            Directory.CreateDirectory(salesTotalDir);
+
+            var salesFiles = FindFiles(storesDirectory);
+            var salesTotal = CalculateSalesTotal(salesFiles);
+
+            File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
+
+        }
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach (var file in foundFiles)
+            {
+                var extension = Path.GetExtension(file);
+                // The file name will contain the full path, so only check the end of it
+                if (file.EndsWith(".json"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+
+            return salesFiles;
+        }
+        static double CalculateSalesTotal(IEnumerable<string> salesFiles)
+        {
+            double salesTotal = 0;
+
+            // READ FILES LOOP
+            // Loop over each file path in salesFiles
+            foreach (var file in salesFiles)
+            {
+                // Read the contents of the file
+                string salesJson = File.ReadAllText(file);
+
+                // Parse the contents as JSON
+                SalesData data = JsonConvert.DeserializeObject<SalesData>(salesJson);
+
+                // Add the amount found in the Total field to the salesTotal variable
+                salesTotal += data.Total;
+            }
+
+            return salesTotal;
+        }
+
+    }
+    class SalesData
+    {
+        public double Total { get; set; }
+    }
+}
---
+++ challenges/fibonacci/mrf/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mrf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter a number  ");
+            string n = Console.ReadLine();
+            bool Next = Int32.TryParse(n, out number);
+            Console.Write(String.Format("fibonacci({0}) = ", n));
+            Console.Write(fibonacci(number));
+        }
+
+        static long fibonacci(int n)
+        {
+            long[] result = new long[n + 2];
+            result[0] = 0;
+            result[1] = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result[i] = result[i - 1] + result[i - 2];
+            }
+            return result[n];
+        }
+        /*         static int fibonacci(int n)
+                {
+                    if (n <= 1)
+                        return n;
+                    return fibonacci(n - 1) + fibonacci(n - 2);
+                } */
+
+    }
+}
